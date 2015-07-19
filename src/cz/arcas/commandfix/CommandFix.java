d5363a923e2bdf7aa24f78bb24905b9f066a2f39
Arcas <Arcas@192.168.0.102>
2015-07-19 14:20:17 +0200

    First relase on Git!

---
+++ src/cz/arcas/commandfix/CommandFix.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package cz.arcas.commandfix;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ *
+ * @author Arcas
+ */
+public final class CommandFix extends JavaPlugin implements Listener {
+    
+    @Override
+    public void onEnable(){
+        getLogger().info("Loaded");
+        getLogger().info("For Uranus-Portal.com by ArcasCZ!");
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+    
+    @Override
+    public void onDisable(){
+        getLogger().info("Unloaded!");
+        getLogger().info("For Uranus-Portal.com by ArcasCZ!");         
+    }
+    
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    public void onPlayerCommandPreprocessEvent(final PlayerCommandPreprocessEvent e){
+        String message = e.getMessage().toLowerCase();
+        String player = e.getPlayer().getName();
+        if(message.startsWith("/login ") || message.startsWith("/l ") || message.startsWith("/reg ") || message.startsWith("/changepassword ") || message.startsWith("/unregister ") || message.startsWith("/authme register ") || message.startsWith("/authme changepassword ") || message.startsWith("/authme reg ") || message.startsWith("/authme cp ") || message.startsWith("/register ")){
+            System.out.println(player + " issued AuthMe command.");
+        }else if (message.startsWith("/restartsrv") || message.startsWith("/holdsrv") || message.startsWith("/reschedulerestart") || message.startsWith("/stopwrapper")){
+            System.out.println(player + " issued Wrapper command.");
+        }else{
+            System.out.println(player + " issued server command: " + message);
+        }
+    }
+}
